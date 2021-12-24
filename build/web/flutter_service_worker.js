0991bc961df23c7e2bc77ae9a35a759f98762b41
Fabián Karaben <fabiankaraben@gmail.com>
2021-12-24 14:14:09 -0300

    b

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "91e1cde99b6a3c9f1b486f6db0967823",
-"index.html": "c0a55414ed36689e877e5d519812ae32",
-"/": "c0a55414ed36689e877e5d519812ae32",
+"index.html": "e3dd3cfdcf3e57b5f3ca76b01eb0f539",
+"/": "e3dd3cfdcf3e57b5f3ca76b01eb0f539",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
@@ -16,7 +16,7 @@ const RESOURCES = {
 "canvaskit/canvaskit.wasm": "04ed3c745ff1dee16504be01f9623498",
 "canvaskit/profiling/canvaskit.js": "f3bfccc993a1e0bfdd3440af60d99df4",
 "canvaskit/profiling/canvaskit.wasm": "a9610cf39260f60fbe7524a785c66101",
-"manifest.json": "9445606212e1feb247d4bad903193917",
+"manifest.json": "3bd5f46ba6d40925b1b7495eea5352e3",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
